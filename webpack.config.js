291faddbcb49fed9e80c88fc0b62b2155180382c
Leon Lu <leonchina@aliyun.com>
2016-09-16 22:50:45 +0800

    webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  entry: './js/index.js',
+
+  output: {
+    filename: 'bundle.js',
+    path: './dest'
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
+    ]
+  }
+}
